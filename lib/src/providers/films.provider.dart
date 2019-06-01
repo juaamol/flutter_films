84fd468bd493f23184d0f3a64ea52ba22e7f1e44
juaamol <jambulud@gmail.com>
2019-06-01 11:33:18 +0200

    [UPDATE] - films provider with stream sink

--- lib/src/providers/films.provider.dart
+++ lib/src/providers/films.provider.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter_films/secretdata/secretdata.dart';
@@ -10,17 +11,36 @@ class FilmsProvider {
   String _pathNowPlaying = '/3/movie/now_playing';
   String _pathPopulares = '/3/movie/popular';
   String _language = 'en-US';
+  int _popularPage = 0;
+
+  List<Film> _popular = new List(); 
+
+  final StreamController<List<Film>> _popularStreamController = StreamController<List<Film>>.broadcast();
+
+  Function(List<Film>) get popularSink => _popularStreamController.sink.add;
+
+  Stream<List<Film>> get popularStream => _popularStreamController.stream;
+
+  void disposeStreams(){
+    _popularStreamController?.close();
+  }
 
   Future<List<Film>> _getFilms(String path) async{
+    _popularPage++;
+
     final url = Uri.https(_url, path, {
       'api_key': _apiKey,
       'language': _language,
+      'page': _popularPage.toString()
     });
 
     final resp = await http.get(url);
     final decodedData = json.decode(resp.body);
     final films = new Films.fromJsonList(decodedData['results']);
     
+    _popular.addAll(films.items);
+    popularSink(_popular);
+
     return films.items;
   }
 
