e4b0e75ed5f95e6e00f18a107d6a206ee918fe0c
juaamol <jambulud@gmail.com>
2019-06-01 17:50:32 +0200

    [UPDATE] - detail view description and title

--- lib/src/pages/film_detail.page.dart
+++ lib/src/pages/film_detail.page.dart
@@ -11,7 +11,15 @@ class FilmDetail extends StatelessWidget {
       body: CustomScrollView(
         slivers: <Widget>[
           _createAppBar(film),
-          SliverFillRemaining(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(
+                height: 10.0,
+              ),
+              _posterTitle(context, film),
+              _description(film),
+            ]),
+          ),
         ],
       ),
     );
@@ -26,14 +34,66 @@ class FilmDetail extends StatelessWidget {
       pinned: true,
       flexibleSpace: FlexibleSpaceBar(
         centerTitle: true,
-        title: Text(film.title, style: TextStyle(color: Colors.white, fontSize: 16.0,),),
-        background: FadeInImage(
-          image: NetworkImage(film.getBackgroundImg()),
-          placeholder: AssetImage('assets/img/loading.gif'),
-          fadeInDuration: Duration(milliseconds: 250),
-          fit: BoxFit.cover
+        title: Text(
+          film.title,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16.0,
+          ),
         ),
+        background: FadeInImage(
+            image: NetworkImage(film.getBackgroundImg()),
+            placeholder: AssetImage('assets/img/loading.gif'),
+            fadeInDuration: Duration(milliseconds: 250),
+            fit: BoxFit.cover),
       ),
     );
+  }
+
+  Widget _posterTitle(BuildContext context, Film film) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Image(
+                  image: NetworkImage(film.getPosterImg()), height: 150.0)),
+          SizedBox(
+            width: 20.0,
+          ),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  film.title,
+                  style: Theme.of(context).textTheme.title,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  film.originalTitle,
+                  style: Theme.of(context).textTheme.subhead,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.star_border),
+                    Text(film.voteAverage.toString(),
+                        style: Theme.of(context).textTheme.subhead),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _description(Film film) {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+        child: Text(film.overview, textAlign: TextAlign.justify));
   }
 }
